40574f84f076806166e5725d9fdc294ce54d84b5
TakumiHendricksDev <takumihendricksdev@gmail.com>
2023-11-04 12:00:01 -0500

    Handle room script and camera movement

--- HackKStateGame/Assets/Scripts/GameManager.cs
+++ HackKStateGame/Assets/Scripts/GameManager.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public FileManager fileManager;
+    public Transform cameraPos;
+    public Room[] rooms = new Room[0];
+    public float transitionSpeed = 2.0f;
+    public Transform player;
+
+    Room inRoom;
+
+    private void MoveCamera() {
+        foreach (Room room in rooms) {
+            if (room.collEnter) {
+                inRoom = room;
+                Vector3 targetPos = new Vector3(room.closestRoom(player).position.x, cameraPos.position.y, cameraPos.position.z);
+                cameraPos.position = Vector3.Lerp(cameraPos.position, targetPos, transitionSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void Update() {
+        MoveCamera();
+    }
 }
